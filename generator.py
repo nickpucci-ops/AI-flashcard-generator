81963b5c9ab095101d00642d9e5841584dd89125
Nick Pucci <nickpucci88@gmail.com>
2025-02-21 11:40:14 -0600

    simplify llama system prompt for now so we can test output

--- generator.py
+++ generator.py
@@ -4,7 +4,7 @@ model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
 model.load()
 
 system_prompt = """
-You are a flashcard generator. The user will provide a question or topic in which they would like a concise and simple description or definition about the specific concept. Keep your response short. It should only be a factual description or definition. If it's related to an equation or a formula, simply provide the equation or formula. If the user provides a question, try to summarize it into one name or title. For example: "What is endomorphisis?", the topic of the card should say "Endomorphisis", and the definition should be a short description that answers the question. All definitions must be accurate
+You are a flashcard generator. The user will provide a topic or question, and you will generate a concise definition or explanation. Keep your response short and factual.
 """
 user_input = ""
 
