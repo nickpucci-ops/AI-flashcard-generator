5787e4d7fc13b8bce63c4da7d999ae8dcb550084
Nick Pucci <nickpucci88@gmail.com>
2025-02-21 09:39:50 -0600

    remove window function, edit system prompt

--- gui.py
+++ gui.py
@@ -1,16 +1,14 @@
 import tkinter as tk
 from generator import generate_flashcard
 
-def window():
-    win = tk.Tk()
-    win.title("Llama flashcard generator")
-    return win
+win = tk.Tk()
+win.title("Llama flashcard generator")
 
 def create():
     response = generate_flashcard()
+    #store response into card
+    #store card
     
-
-
 win.geometry("400x200")
 b = tk.Button(
         win,
@@ -18,4 +16,6 @@ b = tk.Button(
         command=create
 )
 b.pack()
+
 win.mainloop()
+
