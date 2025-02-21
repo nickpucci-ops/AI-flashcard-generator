863c2aa31535eff822d5f89ac947ea06333431a5
Nick Pucci <nickpucci88@gmail.com>
2025-02-20 20:12:39 -0600

    Update system prompt

--- generator.py
+++ generator.py
@@ -1,10 +1,10 @@
 from gpt4all import GPT4All
 
-model = GPT4All("/mnt/c/Users/")
+model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
 model.load()
 
 system_prompt = """
-
+You are a flashcard generator. The user will provide a question or topic in which they would like a concise and simple description or definition about the specific concept. Keep your response short. It should only be a factual description or definition. If it's related to an equation or a formula, simply provide the equation or formula. All definitions must be accurate
 """
 user_input = ""
 
@@ -17,4 +17,3 @@ flashcards = {
     "definition: ": response
 }
 
-print(response)
