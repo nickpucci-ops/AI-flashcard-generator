81e8f7205f254285d8b61123dfb7ee76a77345e5
Nick Pucci <nickpucci88@gmail.com>
2025-02-21 20:51:24 -0600

    Update test.py, remove placeholder

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from generator import generate_flashcard
 
 print("Testing Llama output\n")
-test_input = "What is endothermic process?" 
+test_input = "" 
 response = generate_flashcard(test_input)
 
 print("Response: \n" + response)
